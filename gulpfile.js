a96607d121e7ea499bb655e3c480c804a588911b
Franck Cornu <franck.cornu@outlook.com>
2019-02-21 15:58:33 -0500

    * Added update-version gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,26 @@ build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not came
 
 build.initialize(gulp);
 
-gulp.task('version-sync', function () {
- 
-    // import gulp utilits to write error messages
+gulp.task('update-version', () => {
     const gutil = require('gulp-util');
-   
-    // import file system utilities form nodeJS
     const fs = require('fs');
-   
-    // read package.json
-    var pkgConfig = require('./package.json');
-   
-    // read configuration of web part solution file
-    var pkgSolution = require('./config/package-solution.json');
-   
-    // log old version
-    gutil.log('Old Version:\t' + pkgSolution.solution.version);
-   
-    // Generate new MS compliant version number
-    var newVersionNumber = pkgConfig.version.split('-')[0] + '.0';
-   
-    // assign newly generated version number to web part version
-    pkgSolution.solution.version = newVersionNumber;
-   
-    // log new version
-    gutil.log('New Version:\t' + pkgSolution.solution.version);
-   
-    // write changed package-solution file
-    fs.writeFile('./config/package-solution.json', JSON.stringify(pkgSolution, null, 4));   
+    const semver = require('semver')
+
+    if (process.argv.indexOf('--version') !== -1 && semver.valid(process.argv[4])) {
+                
+        const pkgSolution = require('./config/package-solution.json');
+        const newVersionNumber = process.argv[4] + '.0';
+        
+        gutil.log('Old Version:\t' + pkgSolution.solution.version);
+        
+        pkgSolution.solution.version = newVersionNumber;
+        gutil.log('New Version:\t' + pkgSolution.solution.version);
+
+        fs.writeFile('./config/package-solution.json', JSON.stringify(pkgSolution, null, 4));
+
+    } else {
+        gutil.log('The provided version is not a valid SemVer version');
+    }
+
+    return Promise.resolve();
 });
